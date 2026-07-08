7e4cc6068e5c245575f799d55ae2c25d197d582f
aj1a0o0o <houblehh@gmail.com>
2026-07-08 18:14:51 +0800

    Add methods to manage and retrieve rows in RowContainer part 2

--- src/01_UI/UI_RowContainer.jsx
+++ src/01_UI/UI_RowContainer.jsx
@@ -212,3 +212,243 @@ function()
         this.group.layout.resize();
     }
 };
+//==================================================
+// Get Row Count
+//==================================================
+
+UI.RowContainer.prototype.getCount =
+function()
+{
+    return this.rows.length;
+};
+
+
+//==================================================
+// Get Row
+//==================================================
+
+UI.RowContainer.prototype.getRow =
+function(index)
+{
+    if(index < 0)
+    {
+        return null;
+    }
+
+    if(index >= this.rows.length)
+    {
+        return null;
+    }
+
+    return this.rows[index];
+};
+
+
+//==================================================
+// Get Rows
+//==================================================
+
+UI.RowContainer.prototype.getRows =
+function()
+{
+    return this.rows;
+};
+
+
+//==================================================
+// Get Data
+//==================================================
+
+UI.RowContainer.prototype.getData =
+function()
+{
+    var data = [];
+
+    var i;
+
+    for(i = 0; i < this.rows.length; i++)
+    {
+        data.push(
+            this.rows[i].getData()
+        );
+    }
+
+    return data;
+};
+
+
+//==================================================
+// Set Data
+//==================================================
+
+UI.RowContainer.prototype.setData =
+function(data)
+{
+    var i;
+
+    this.clear();
+
+    if(data == null)
+    {
+        this.createDefaultRows();
+        return;
+    }
+
+    for(i = 0; i < data.length; i++)
+    {
+        var row = this.addRow(false);
+
+        row.setData(data[i]);
+    }
+
+    while(this.rows.length < UI.MIN_ROWS)
+    {
+        this.addRow(false);
+    }
+
+    this.refreshButtons();
+
+    this.refreshLayout();
+};
+
+
+//==================================================
+// Get Total Probability
+//==================================================
+
+UI.RowContainer.prototype.getTotalProbability =
+function()
+{
+    var total = 0;
+
+    var i;
+
+    for(i = 0; i < this.rows.length; i++)
+    {
+        total += this.rows[i].getProbability();
+    }
+
+    return total;
+};
+
+
+//==================================================
+// Enable
+//==================================================
+
+UI.RowContainer.prototype.enable =
+function()
+{
+    var i;
+
+    for(i = 0; i < this.rows.length; i++)
+    {
+        this.rows[i].enable();
+    }
+};
+
+
+//==================================================
+// Disable
+//==================================================
+
+UI.RowContainer.prototype.disable =
+function()
+{
+    var i;
+
+    for(i = 0; i < this.rows.length; i++)
+    {
+        this.rows[i].disable();
+    }
+};
+
+
+//==================================================
+// Clear
+//==================================================
+
+UI.RowContainer.prototype.clear =
+function()
+{
+    while(this.rows.length > 0)
+    {
+        this.rows[0].remove();
+
+        this.rows[0].destroy();
+
+        this.rows.splice(0, 1);
+    }
+
+    this.refreshLayout();
+};
+
+
+//==================================================
+// Validate
+//==================================================
+
+UI.RowContainer.prototype.validate =
+function()
+{
+    var i;
+
+    for(i = 0; i < this.rows.length; i++)
+    {
+        if(!this.rows[i].validateProbability())
+        {
+            return false;
+        }
+    }
+
+    return true;
+};
+
+
+//==================================================
+// Refresh
+//==================================================
+
+UI.RowContainer.prototype.refresh =
+function()
+{
+    this.refreshButtons();
+
+    this.refreshLayout();
+};
+
+
+//==================================================
+// Destroy
+//==================================================
+
+UI.RowContainer.prototype.destroy =
+function()
+{
+    this.clear();
+
+    this.rows = [];
+
+    this.group = null;
+
+    this.parent = null;
+
+    this.onRowAdded = null;
+
+    this.onRowRemoved = null;
+
+    this.onChanged = null;
+};
+
+
+//==================================================
+// Debug
+//==================================================
+
+UI.RowContainer.prototype.toString =
+function()
+{
+    return "[RowContainer : "
+        + this.rows.length
+        + " rows]";
+};
